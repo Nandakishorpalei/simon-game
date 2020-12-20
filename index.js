f93d4b876835b5f6a58e954c61e68c57cbe9a1a7
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2020-12-20 19:37:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+$("#green").click(function(){
+  var audio=new Audio("sounds/green.mp3");
+  audio.play();
+  $("body").addClass("redcolor");
+  $("#level-title").text("Game Over, Press Any Key To Restart");
+  setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
+});
+
+$("#red").click(function(){
+  var audio=new Audio("sounds/red.mp3");
+  audio.play();
+  $("body").addClass("redcolor");
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+  setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
+});
+
+$("#yellow").click(function(){
+  var audio=new Audio("sounds/yellow.mp3");
+  audio.play();
+  $("body").addClass("redcolor");
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+  setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
+});
+
+$("#blue").click(function(){
+  var audio=new Audio("sounds/blue.mp3");
+  audio.play();
+  $("body").addClass("redcolor");
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+  setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
+});
