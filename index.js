1712f0aa3362d82ea1d8fd03136a1220346d5e50
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2020-12-21 10:29:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,35 +2,38 @@
 $("#green").click(function(){
   var number=Math.floor(Math.random() * 101);
 if(number%5==0){
-    var audio=new Audio("wrong.mp3");
+    var audio=new Audio("sounds/wrong.mp3");
     audio.play();
     $("#level-title").text("Oops Number " +number+", Game Over Press Any Key To Restart");
-    setTimeout(function(){$("#level-title").text("Press A Key to Start");},3000);
+    setTimeout(function(){$("#level-title").text("Press A Key to Start");},2000);
   }
   else{
-  var audio=new Audio("green.mp3");
+  var audio=new Audio("sounds/green.mp3");
   audio.play();
-  var audio=new Audio("blue.mp3");
+  var audio=new Audio("sounds/blue.mp3");
   audio.play();
   $("body").addClass("redcolor");
+  $("#level-title").text("Number " +number+", Press A Color To Continue");
   setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
+
     }
 });
 
 $("#red").click(function(){
   var number=Math.floor(Math.random() * 101);
 if(number%5==0){
-    var audio=new Audio("wrong.mp3");
+    var audio=new Audio("sounds/wrong.mp3");
     audio.play();
     $("#level-title").text("Oops Number " +number+", Game Over Press Any Key To Restart");
-    setTimeout(function(){$("#level-title").text("Press A Key to Start");},3000);
+    setTimeout(function(){$("#level-title").text("Press A Key to Start");},2000);
   }
   else{
-  var audio=new Audio("red.mp3");
+  var audio=new Audio("sounds/red.mp3");
   audio.play();
-  var audio=new Audio("yellow.mp3");
+  var audio=new Audio("sounds/yellow.mp3");
   audio.play();
   $("body").addClass("redcolor");
+    $("#level-title").text("Number " +number+", Press A Color To Continue");
   setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
 }
 });
@@ -38,17 +41,18 @@ if(number%5==0){
 $("#yellow").click(function(){
   var number=Math.floor(Math.random() * 101);
 if(number%5==0){
-    var audio=new Audio("wrong.mp3");
+    var audio=new Audio("sounds/wrong.mp3");
     audio.play();
     $("#level-title").text("Oops Number " +number+", Game Over Press Any Key To Restart");
-    setTimeout(function(){$("#level-title").text("Press A Key to Start");},3000);
+    setTimeout(function(){$("#level-title").text("Press A Key to Start");},2000);
   }
   else{
-  var audio=new Audio("green.mp3");
+  var audio=new Audio("sounds/green.mp3");
   audio.play();
-  var audio=new Audio("red.mp3");
+  var audio=new Audio("sounds/red.mp3");
   audio.play();
   $("body").addClass("redcolor");
+    $("#level-title").text("Number " +number+", Press A Color To Continue");
   setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
 }
 });
@@ -56,17 +60,18 @@ if(number%5==0){
 $("#blue").click(function(){
   var number=Math.floor(Math.random() * 101);
 if(number%5==0){
-    var audio=new Audio("wrong.mp3");
+    var audio=new Audio("sounds/wrong.mp3");
     audio.play();
     $("#level-title").text("Oops Number " +number+", Game Over Press Any Key To Restart");
-    setTimeout(function(){$("#level-title").text("Press A Key to Start");},3000);
+    setTimeout(function(){$("#level-title").text("Press A Key to Start");},2000);
   }
   else{
-  var audio=new Audio("blue.mp3");
+  var audio=new Audio("sounds/blue.mp3");
   audio.play();
-  var audio=new Audio("yellow.mp3");
+  var audio=new Audio("sounds/yellow.mp3");
   audio.play();
   $("body").addClass("redcolor");
+    $("#level-title").text("Number " +number+", Press A Color To Continue");
   setTimeout(function(){ $("body").removeClass("redcolor");}, 100);
 }
 });
